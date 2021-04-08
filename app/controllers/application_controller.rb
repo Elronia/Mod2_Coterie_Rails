340dad49ab534ee885f3687d1f331db0105b0f91
Elronia <zar.catherine@gmail.com>
2021-04-07 22:25:37 -0400

    add current user helper method and fix routes on menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 class ApplicationController < ActionController::Base
 
 
-    helper_method :check_if_user_is_logged_in
+    helper_method :current_user, :check_if_user_is_logged_in
     before_action :authorized_to_see_page
 
-    def set_current_user
-        @current_user = User.find_by(id: session[:user_id])
+    # def set_current_user
+    #     @current_user = User.find_by(id: session[:user_id])
+    # end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 
     def check_if_user_is_logged_in
-        !set_current_user.nil?
+        !current_user.nil?
     end
 
     def authorized_to_see_page
