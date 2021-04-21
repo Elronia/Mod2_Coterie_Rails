d6daf874cc6b6ac36062e11a307da236d001ce84
Elronia <zar.catherine@gmail.com>
2021-04-21 18:11:40 -0400

    added a new validation allowing the user be a host but not an attendee

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,13 @@
 class Registration < ApplicationRecord
   belongs_to :user
   belongs_to :meeting
+  validate :host_cannot_be_attendee
+
+  private
+
+  def host_cannot_be_attendee
+    if user == meeting.user
+      errors.add(:base, "Host cannot be an attendee")
+    end
+  end
 end
